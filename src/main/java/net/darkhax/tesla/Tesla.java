eaa0d09c166931861a8b10e713dd2579f039c587
Tyler Hancock <darklime@live.ca>
2016-05-02 06:19:22 -0600

    Removed uneeded proxy code.

--- src/main/java/net/darkhax/tesla/Tesla.java
+++ src/main/java/net/darkhax/tesla/Tesla.java
@@ -1,23 +1,11 @@
 package net.darkhax.tesla;
 
-import net.darkhax.tesla.common.CommonProxy;
 import net.darkhax.tesla.lib.Constants;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = Constants.MCVERSION)
 public class Tesla {
     
-    @SidedProxy(clientSide = Constants.CLIENT_PROXY_CLASS, serverSide = Constants.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy;
-    
     @Mod.Instance(Constants.MOD_ID)
     public static Tesla instance;
-    
-    @EventHandler
-    public void preInit (FMLPreInitializationEvent pre) {
-        
-    }
 }
--- src/main/java/net/darkhax/tesla/common/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package net.darkhax.tesla.common;
-
-public class CommonProxy {
-    
-    public void preInit () {
-    
-    }
-}
