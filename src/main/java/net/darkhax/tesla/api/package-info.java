fdb7695735042d5e124ae04269780d6290f5c93d
Tyler Hancock <darklime@live.ca>
2016-05-02 06:50:52 -0600

    Updated API info.

---
+++ src/main/java/net/darkhax/tesla/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = "Tesla", provides = "Tesla|API", apiVersion = Constants.VERSION_NUMBER)
+package net.darkhax.tesla.api;
+
+import net.minecraftforge.fml.common.API;
+import net.darkhax.tesla.lib.Constants;
