ffcf24fb7a9bee93151ac36cecc3bd9957018b65
Tyler Hancock <darklime@live.ca>
2016-05-03 01:38:10 -0600

    Fixed a capability derp.

--- src/main/java/net/darkhax/tesla/capability/TeslaStorage.java
+++ src/main/java/net/darkhax/tesla/capability/TeslaStorage.java
@@ -5,8 +5,12 @@ import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.Capability.IStorage;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 
 public class TeslaStorage<T extends ITeslaHandler> implements IStorage<ITeslaHandler> {
+    
+    @CapabilityInject(ITeslaHandler.class)
+    public static Capability<ITeslaHandler> TESLA_HANDLER_CAPABILITY = null;
     
     @Override
     public NBTBase writeNBT (Capability<ITeslaHandler> capability, ITeslaHandler instance, EnumFacing side) {
