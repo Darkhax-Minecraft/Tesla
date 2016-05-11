23ed7e122ed1c8ae640c289e47dc2dd1f00b822f
Tyler Hancock <darklime@live.ca>
2016-05-11 06:03:14 -0600

    Split capabilities into their 3 roles.
    Closes #10 and #5

--- src/main/java/net/darkhax/tesla/api/InfiniteTeslaConsumer.java
+++ src/main/java/net/darkhax/tesla/api/InfiniteTeslaConsumer.java
@@ -3,26 +3,36 @@ package net.darkhax.tesla.api;
 import net.minecraft.util.EnumFacing;
 
 /**
- * An implementation of the TeslaContainer that will consume Tesla power indefinitely. This
- * implementation can not send power.
+ * Logic for a Tesla Consumer that will consume infinite amounts of power.
+ * 
+ * As of 1.0.2.x the TeslaContainer has been deprecated. It will be removed in 1.0.3.x
  */
-public class InfiniteTeslaConsumer extends TeslaContainer {
+public class InfiniteTeslaConsumer extends TeslaContainer implements ITeslaConsumer {
     
     @Override
+    @Deprecated
     public long givePower (long tesla, EnumFacing side, boolean simulated) {
         
         return tesla;
     }
     
     @Override
+    @Deprecated
     public long takePower (long tesla, EnumFacing side, boolean simulated) {
         
         return 0;
     }
     
     @Override
+    @Deprecated
     public boolean isOutputSide (EnumFacing side) {
         
         return false;
+    }
+    
+    @Override
+    public long givePower (long power, boolean simulated) {
+        
+        return power;
     }
 }
