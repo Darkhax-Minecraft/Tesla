66f3218e295d8e649ba66762bca692d883ab5483
iLexiconn <ilexiconn@gmail.com>
2016-05-11 13:34:10 +0200

    Fixed formatting Thanks Obama

--- src/main/java/net/darkhax/tesla/api/ITeslaHandler.java
+++ src/main/java/net/darkhax/tesla/api/ITeslaHandler.java
@@ -15,7 +15,7 @@ public interface ITeslaHandler extends INBTSerializable<NBTBase> {
      * @param side The side being checked.
      * @return The amount of tesla power stored by the handler.
      */
-    long getStoredPower(EnumFacing side);
+    long getStoredPower (EnumFacing side);
 
     /**
      * Gets the maximum amount of tesla power that the handler can hold.
@@ -23,27 +23,27 @@ public interface ITeslaHandler extends INBTSerializable<NBTBase> {
      * @param side The side being checked.
      * @return The maximum amount of tesla power that can be held by the handler.
      */
-    long getCapacity(EnumFacing side);
+    long getCapacity (EnumFacing side);
 
     /**
      * Takes power from the handler by removing it from an internal source.
      *
-     * @param power     The amount of power to request from the handler.
-     * @param side      The side being checked.
+     * @param power The amount of power to request from the handler.
+     * @param side The side being checked.
      * @param simulated Whether or not this is being called as part of a simulation.
      * @return The amount of power that was actually taken.
      */
-    long takePower(long power, EnumFacing side, boolean simulated);
+    long takePower (long power, EnumFacing side, boolean simulated);
 
     /**
      * Provides the handler with a bit of power.
      *
-     * @param power     The amount of power to provide the handler with.
-     * @param side      The side being checked.
+     * @param power The amount of power to provide the handler with.
+     * @param side The side being checked.
      * @param simulated Whether or not this is being called as part of a simulation.
      * @return The amount of power that was accepted by the handler.
      */
-    long givePower(long power, EnumFacing side, boolean simulated);
+    long givePower (long power, EnumFacing side, boolean simulated);
 
     /**
      * Checks if a given side is a valid input face..
@@ -51,7 +51,7 @@ public interface ITeslaHandler extends INBTSerializable<NBTBase> {
      * @param side The side being checked.
      * @return Whether or not the side is a valid input side.
      */
-    boolean isInputSide(EnumFacing side);
+    boolean isInputSide (EnumFacing side);
 
     /**
      * Checks if a given side is a valid output face.
@@ -59,5 +59,5 @@ public interface ITeslaHandler extends INBTSerializable<NBTBase> {
      * @param side The side being checked.
      * @return Whether or not the side is a valid output side.
      */
-    boolean isOutputSide(EnumFacing side);
+    boolean isOutputSide (EnumFacing side);
 }
