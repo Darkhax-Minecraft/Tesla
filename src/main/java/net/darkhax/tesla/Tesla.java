8157a351be8d3375f02dc264c7640a4286a6ba3f
Tyler Hancock <darklime@live.ca>
2017-09-24 17:12:35 -0600

    Added jar signing.

--- src/main/java/net/darkhax/tesla/Tesla.java
+++ src/main/java/net/darkhax/tesla/Tesla.java
@@ -1,5 +1,8 @@
 package net.darkhax.tesla;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.darkhax.tesla.api.ITeslaConsumer;
 import net.darkhax.tesla.api.ITeslaHolder;
 import net.darkhax.tesla.api.ITeslaProducer;
@@ -10,16 +13,25 @@ import net.darkhax.tesla.capability.TeslaCapabilities.CapabilityTeslaProducer;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "tesla", name = "Tesla", version = "@VERSION@")
+@Mod(modid = "tesla", name = "Tesla", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
 public class Tesla {
 
+    private static final Logger LOG = LogManager.getLogger("Tesla");
+    
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
         CapabilityManager.INSTANCE.register(ITeslaConsumer.class, new CapabilityTeslaConsumer<>(), BaseTeslaContainer.class);
         CapabilityManager.INSTANCE.register(ITeslaProducer.class, new CapabilityTeslaProducer<>(), BaseTeslaContainer.class);
         CapabilityManager.INSTANCE.register(ITeslaHolder.class, new CapabilityTeslaHolder<>(), BaseTeslaContainer.class);
+    }
+    
+    @EventHandler
+    public void onFingerprintViolation (FMLFingerprintViolationEvent event) {
+
+        LOG.error("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
